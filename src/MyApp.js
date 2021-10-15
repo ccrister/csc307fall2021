1159f347f58c11d3de00f416727516ff5b960aca
chris <ccrister@calpoly.edu>
2021-10-14 17:09:28 -0700

    frontend stuff

--- src/MyApp.js
+++ src/MyApp.js
@@ -1,6 +1,8 @@
 import Form from './Form';
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import Table from './Table';
+
+import axios from 'axios'
 
 
 const characters = [
@@ -26,10 +28,39 @@ const characters = [
 
 function MyApp() {
   const [characters, setCharacters] = useState([]);
-
-  function updateList(person) {
-  setCharacters([...characters, person]);
-  }
+  function updateList(person) { 
+    makePostCall(person).then( result => {
+    if (result)
+       setCharacters([...characters, person] );
+    });
+ }
+  async function makePostCall(person){
+    try {
+       const response = await axios.post('http://localhost:5000/users', person);
+       return response;
+    }
+    catch (error) {
+       console.log(error);
+       return false;
+    }
+ }
+  async function fetchAll(){
+    try {
+       const response = await axios.get('http://localhost:5000/users');
+       return response.data.users_list;     
+    }
+    catch (error){
+       //We're not handling errors. Just logging into the console.
+       console.log(error); 
+       return false;         
+    }
+ }
+ useEffect(() => {
+   fetchAll().then( result => {
+      if (result)
+         setCharacters(result);
+    });
+}, [] );
   return (
     <div className="container">
       <Table characterData={characters} removeCharacter={removeOneCharacter} />
