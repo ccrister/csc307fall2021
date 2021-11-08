8c42c59fb109f135f674ed882b89956f46285e3b
ccrister <71282567+ccrister@users.noreply.github.com>
2021-11-07 18:50:08 -0800

    Update MyApp.js

--- src/MyApp.js
+++ src/MyApp.js
@@ -4,6 +4,27 @@ import Table from './Table';
 
 
 
+const characters = [
+      {
+        name: 'Charlie',
+        job: 'Janitor',
+      },
+      {
+        name: 'Mac',
+        job: 'Bouncer',
+      },
+      {
+        name: 'Dee',
+        job: 'Aspring actress',
+      },
+      {
+        name: 'Dennis',
+        job: 'Bartender',
+      },
+];
+
+
+
 
 
 
